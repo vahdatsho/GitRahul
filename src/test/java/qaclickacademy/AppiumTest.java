d1a7214a01bdc5302e33c398f952e748302fe285
vahdat <vahdatsho@gmail.com>
2020-02-05 14:08:46 -0800

    My first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAppAndroid() {
+		System.out.println("NativeAppAndroid");
+	}
+	
+	@Test 
+	public void IOSApps() {
+		System.out.println("IOSApps");	
+	}
+
+}
